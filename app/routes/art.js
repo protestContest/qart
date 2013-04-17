28cf094c9e86f8ac69fd58c4b029b6c5f72b3b44
Zack Michener <zack@zackmichener.net>
2013-04-16 20:35:59 -0400

    add admin views

--- app/routes/art.js
+++ app/routes/art.js
@@ -20,7 +20,16 @@ exports.view = function(req, res) {
 };
 
 exports.editForm = function(req, res) {
-
+    res.render('edit', {
+        'piece': {
+            'title': 'Piece Title',
+            'artist': {
+                'name': 'Artist O. Piece',
+                'about': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Donec rhoncus dignissim lacus vulputate vestibulum. Integer pulvinar velit sed quam pharetra vel malesuada erat blandit.  Morbi ligula ipsum, malesuada ut porta in, rhoncus sed neque.  Nullam at est dignissim sem tristique laoreet. Fusce at ullamcorper justo. In hac habitasse platea dictumst. In sagittis, tortor et volutpat viverra, risus nisl fermentum tellus, eget volutpat est nisi vel sapien. Maecenas convallis tristique metus non iaculis.'
+            },
+            'description': 'Piece description'
+        }
+    });
 };
 
 exports.update = function(req, res) {
